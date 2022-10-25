82b700c0e6a9e32ccef1046e87021a800d31ebfd
Eilidh <56912943+SMC242@users.noreply.github.com>
2022-10-25 13:43:59 +0100

    WIP: pathways for each request type on client

--- client.py
+++ client.py
@@ -1,8 +1,62 @@
 from argparse import ArgumentParser
+import socket
+import os
+from math import ceil
+
+from request import make_inital_req, to_fields, validate_ack
+
+# 1 KiB
+PACKET_SIZE = 1024
 
 
-def request(type: str) -> None:
-    pass
+def valid_file(name: str) -> bool:
+    return os.path.exists(name) and os.path.isfile(name)
+
+
+def packets_needed(file_name: str) -> int:
+    return ceil(os.stat(file_name).st_size / PACKET_SIZE)
+
+
+def parse_ack(response: bytes) -> dict | None:
+    """Validate and convert acknowledgement message into its fields"""
+    fields = to_fields(response.decode("utf-8"))
+    return fields if validate_ack(fields) else None
+
+
+def make_requester(ip: str, port: int):
+    """Get a function that will transmit a request of the given type"""
+
+    def request_of(type: str):
+        # The value will either be a file path or True (if --list)
+        def with_arg(arg):
+            if type == "get":
+                code, name, n = 0, arg, 0
+            elif type == "put":
+                if not valid_file(arg):
+                    raise ValueError("Invalid file name")
+                code, name, n = 1, arg, packets_needed(arg)
+            else:
+                code, name, n = 2, "", 0
+            req = make_inital_req(code, name, n)
+
+            # Any exceptions raised will close the connection
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect((ip, port))
+
+                sock.send(req)  # Initial request
+                # Receive acknowledgement
+                ack_fields = parse_ack(sock.recv(PACKET_SIZE))
+                if not ack_fields:
+                    raise ValueError(
+                        "Received invalid response from server. Closing connection..."
+                    )
+                # Report any errors from the server
+                elif ack_fields["status"] == "1":
+                    raise Exception(ack_fields["msg"])
+
+                # TODO: receive/send data
+
+    return request_of
 
 
 def get_args() -> dict:
@@ -33,17 +87,19 @@ def get_args() -> dict:
 
 def main():
     REQUEST_TYPES = ("get", "put", "list")
-    COMMANDS = {
-        "get": lambda: request("get"),
-        "put": lambda: request("put"),
-        "list": lambda: request("list"),
-    }
+
     args = get_args()
-    for k in COMMANDS:
+    request_of = make_requester(ip, port)
+    commands = {t: lambda: request_of(t) for t in REQUEST_TYPES}
+
+    for k in commands:
         # NOTE: empty file paths will fall through this (on purpose)
-        if args[k]:
-            print("executing")
-            return COMMANDS[k]()
+        if args[k] is None:
+            print("Executing command")
+            try:
+                return commands[k](args[k])
+            except Exception as e:
+                return print(e.message)
 
     # No request made
     print(
